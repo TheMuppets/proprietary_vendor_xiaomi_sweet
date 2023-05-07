4b854732bd077dc57ef79f71eb9faafd41dd3845
basamaryan <basam.aryan@gmail.com>
2023-05-07 23:51:37 +0000

    sweet: Import decommonized liboemcrypto
    Change-Id: I33530d5d0e7c62149a52e9d16fff948f6d4a7593

--- sweet-vendor.mk
+++ sweet-vendor.mk
@@ -309,6 +309,7 @@ PRODUCT_PACKAGES += \
     libmulticam_image_optical_zoom \
     libmulticam_video_optical_zoom \
     libnotifyaudiohal \
+    liboemcrypto \
     librelight_only \
     libremosaic_wrapper \
     libremosaiclib \
